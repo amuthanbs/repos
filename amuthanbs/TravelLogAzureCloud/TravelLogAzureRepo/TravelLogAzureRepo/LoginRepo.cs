4b5e8f9029f7d3ee7077921c1853839882a53a6e
nbamuthan <nbamuthan@gmail.com>
2020-11-22 21:46:53 +0530

    All Code Set

---
+++ amuthanbs/TravelLogAzureCloud/TravelLogAzureRepo/TravelLogAzureRepo/LoginRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TravelLogAzureRepo.Login
+{
+    public class LoginRepo
+    {
+        public LoginRepo()
+        {
+
+        }
+        public  bool TryUserName(string userName)
+        {
+            return string.IsNullOrEmpty(userName) ? false : true;
+        }
+    }
+}
---
+++ amuthanbs/TravelLogAzureCloud/TravelLogCloudAzureBL/TravelLogCloudAzureBL/LoginBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TravelLogCloudAzureBL.Login
+{
+    using TravelLogAzureRepo.Login;
+    public class LoginBL
+    {
+        public  LoginBL()
+        {
+            
+        }
+
+        public bool CheckLoginUserName(string userName)
+        {
+            LoginRepo loginRepo = loginRepo = new LoginRepo();
+            return loginRepo.TryUserName(userName);
+        }
+    }
+}
